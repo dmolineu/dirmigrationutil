088d813be9bfae7a426da8a9dbf9fbd7091fd09a
dmolineu <dmolineu@gmail.com>
2019-04-10 22:30:09 -0500

    Added retry to code to reprocess due to .DS_Store exceptions (only happened first time each .DS_Store file was encountered). Added options second target dir to compare against.

--- src/main/java/com/dlmol/dirmigrationutil/DirmigrationutilApplication.java
+++ src/main/java/com/dlmol/dirmigrationutil/DirmigrationutilApplication.java
@@ -1,7 +1,7 @@
 package com.dlmol.dirmigrationutil;
 
-import ch.qos.logback.core.util.FileUtil;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,76 +10,190 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class DirmigrationutilApplication {
+public class DirMigrationUtilApplication {
 
     public static void main(String[] args) throws IOException {
-        args = new String[]{"/Volumes/Temp/D7100/", "/Volumes/Photos/"};
-        SpringApplication.run(DirmigrationutilApplication.class, args);
+        args = new String[]{"/Users/dmolineu/Pictures/", "/Volumes/dls_docs/My Pictures/", "/Volumes/Photos/"};
+        SpringApplication.run(DirMigrationUtilApplication.class, args);
         System.out.println("args: " + Arrays.asList(args).stream().collect(Collectors.joining(", ")));
-        if (args == null || args.length < 2){
+        if (args == null || args.length < 2) {
             System.out.println("Invalid args!");
             return;
         }
-        File source = new File(args[0]);
-        File target = new File(args[1]);
-        compareAndClean(source, target);
+
+        boolean copy = false;
+        if (copy)
+            smartCopy(new File("/Volumes/NIKON D7100/DCIM/"), new File("/Volumes/Temp/D7100/"));
+        else {
+            File source = new File(args[0]);
+            File target = new File(args[1]);
+            File target2 = new File(args[2]);
+            compareAndClean(source, target, target2);
+        }
     }
 
-    private static void compareAndClean(File sourceDir, File targetDir) throws IOException {
-       long startMs = System.currentTimeMillis();
-        if (!sourceDir.isDirectory()){
+    private static void smartCopy(File source, File target) throws IOException {
+        long startMs = System.currentTimeMillis();
+        if (!source.isDirectory()) {
+            System.out.println(source.getAbsolutePath() + " is NOT a valid directory!");
+            return;
+        }
+        if (!target.isDirectory()) {
+            System.out.println(target.getAbsolutePath() + " is NOT a valid directory!");
+            return;
+        }
+        Set<File> sourceFiles = Files.walk(source.toPath())
+                .map(Path::toFile)
+                .filter(f -> f.isFile())
+                .collect(Collectors.toSet());
+        System.out.println("Found " + sourceFiles.size() + " files in source (" + source.getAbsolutePath() + ") in " +
+                (System.currentTimeMillis() - startMs) / 1000 + " seconds.");
+
+        Set<File> targetFiles = Files.walk(target.toPath())
+                .map(Path::toFile)
+                .filter(f -> f.isFile())
+                .collect(Collectors.toSet());
+        System.out.println("Found " + targetFiles.size() + " files in target (" + target.getAbsolutePath() + ") in " +
+                (System.currentTimeMillis() - startMs) / 1000 + " total seconds.");
+
+        sourceFiles.forEach(file -> {
+            File testTarget = new File(target.getAbsolutePath() + File.separator + file.getName());
+            boolean copy = false;
+            if (testTarget.exists()) {
+//                System.out.println("\tTarget file exists: " + testTarget.getAbsolutePath());
+                if (testTarget.length() == 0) {
+                    System.out.println("\tTarget file is empty: " + testTarget.getAbsolutePath());
+                    copy = true;
+                }
+            } else {
+                copy = true;
+            }
+            if (copy) {
+                System.out.println("\tCopying " + file.getAbsolutePath() + " to " + testTarget.getAbsolutePath());
+                try {
+                    FileUtils.copyToDirectory(file, target);
+                } catch (IOException e) {
+                    System.out.println("Unable to copy file '" + file.getAbsolutePath() + "' to '" + target.getAbsolutePath() +
+                            "'. Exception: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        System.out.println("smartCopy(): Done after " + (System.currentTimeMillis() - startMs) / 1000 + " seconds.");
+    }
+
+    private static void compareAndClean(File sourceDir, File targetDir1) throws IOException {
+        compareAndClean(sourceDir, targetDir1, null);
+    }
+
+    private static void compareAndClean(File sourceDir, File targetDir1, File targetDir2) throws IOException {
+        long startMs = System.currentTimeMillis();
+        if (!sourceDir.isDirectory()) {
             System.out.println(sourceDir.getAbsolutePath() + " is NOT a valid directory!");
             return;
         }
-        if (!targetDir.isDirectory()){
-            System.out.println(targetDir.getAbsolutePath() + " is NOT a valid directory!");
+        if (!targetDir1.isDirectory()) {
+            System.out.println(targetDir1.getAbsolutePath() + " is NOT a valid directory!");
             return;
         }
-        System.out.println("Source dir: " + sourceDir.getAbsolutePath() +
-                "\nTarget dir: " + targetDir.getAbsolutePath());
-        Set<File> sourceFiles = Files.walk(sourceDir.toPath())
-                .map(Path::toFile)
-                .filter(f -> f.isFile())
-                .collect(Collectors.toSet());
-        System.out.println("Found " + sourceFiles.size() + " files in source (" + sourceDir.getAbsolutePath() + ")");
+        System.out.println("\nSource dir: " + sourceDir.getAbsolutePath() +
+                "\nTarget dir: " + targetDir1.getAbsolutePath());
+        if (targetDir2 != null)
+            System.out.println("Target dir #2: " + targetDir2.getAbsolutePath() + "\n");
 
-        Set<File> targetFiles = Files.walk(targetDir.toPath())
-                .map(Path::toFile)
-                .filter(f -> f.isFile())
-                .collect(Collectors.toSet());
+        final Predicate<File> imageFilter = f -> f.isFile() &&
+                (FilenameUtils.getExtension(f.getAbsolutePath()).toLowerCase().contains("jpg") ||
+                        FilenameUtils.getExtension(f.getAbsolutePath()).toLowerCase().contains("nef"));
+
+        long ms;
+
+        boolean error = false;
+        Set<File> targetFiles = new HashSet<>();
+        int i = 1;
+        do {
+            System.out.println("Attempt #" + (i++) + " for " + targetDir1.getAbsolutePath());
+            ms = System.currentTimeMillis();
+            try {
+                targetFiles = Files.walk(targetDir1.toPath())
+                        .map(Path::toFile)
+                        .filter(imageFilter)
+                        .collect(Collectors.toSet());
+                System.out.println("Found " + targetFiles.size() + " files in: " + targetDir1.getAbsolutePath() + ", " +
+                        (System.currentTimeMillis() - ms) + " ms.");
+                error = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                error = true;
+            }
+        } while (error);
+
+        if (targetDir2 != null) {
+            ms = System.currentTimeMillis();
+            final Set<File> target2Files = Files.walk(targetDir2.toPath())
+                    .map(Path::toFile)
+                    .filter(imageFilter)
+                    .collect(Collectors.toSet());
+            System.out.println("Found " + target2Files.size() + " files in: " + targetDir2.getAbsolutePath() + ", " +
+                    (System.currentTimeMillis() - ms) + " ms.");
+            targetFiles.addAll(target2Files);
+            System.out.println("Total target file count: " + targetFiles.size());
+        }
+
         Set<String> targetFileNames = targetFiles.stream().map(File::getName).collect(Collectors.toSet());
 
-        System.out.println("Found " + targetFiles.size() + " files in target (" + targetDir.getAbsolutePath() + ")");
+        /*
         Set<File> filesToRemove = sourceFiles.stream()
                 .filter(file -> targetFileNames.contains(file.getName()))
                 .collect(Collectors.toSet());
         System.out.println(filesToRemove.stream().map(File::getAbsolutePath).collect(Collectors.joining("\n")) +
-                "\n" + filesToRemove.size() + " above files to be removed from source.");
+                "\n\n" + filesToRemove.size() + " above files to be removed from source.");
         System.out.println((sourceFiles.size() - filesToRemove.size()) + " remaining files in source.");
+*/
+
+        ms = System.currentTimeMillis();
+        Set<File> sourceFiles = Files.walk(sourceDir.toPath())
+                .map(Path::toFile)
+                .filter(imageFilter)
+                .collect(Collectors.toSet());
+        System.out.println("Found " + sourceFiles.size() + " files in source (" + sourceDir.getAbsolutePath() + "), " +
+                (System.currentTimeMillis() - ms) + " ms.");
 
         Set<File> filesToReview = sourceFiles.stream()
                 .filter(file -> !targetFileNames.contains(file.getName()))
                 .collect(Collectors.toSet());
-        System.out.println(filesToReview.stream().map(File::getAbsolutePath).collect(Collectors.joining("\n")) +
+        System.out.println(filesToReview.stream()
+                .map(File::getAbsolutePath)
+                .sorted()
+                .collect(Collectors.joining("\n")) +
                 "\n" + filesToReview.size() + " above files to be reviewed/moved into collections.");
-        File reviewDir = new File(sourceDir.getAbsolutePath() + File.separator + "review");
+
+        /*
+        File reviewDir = new File(sourceDir.getParentFile().getAbsolutePath() + File.separator + "review");
         if (!reviewDir.isDirectory())
             reviewDir.mkdir();
         System.out.println("Copying " + filesToReview.size() + " files to: " + reviewDir.getAbsolutePath());
         filesToReview.forEach(f -> {
             try {
-                System.out.println("\tCopying " + f.getAbsolutePath() + " to " + reviewDir.getAbsolutePath());
+                System.out.print("\tCopying " + f.getAbsolutePath() + " to " + reviewDir.getAbsolutePath());
+                long ms = System.currentTimeMillis();
                 FileUtils.copyToDirectory(f, reviewDir);
+                final long durationMs = System.currentTimeMillis() - ms;
+                System.out.println("\tDone after " + durationMs + " ms, KBs: " +
+                        f.length() / durationMs);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
-
-        System.out.println("Done after: " + (System.currentTimeMillis() - startMs) + " ms.");
+*/
+        System.out.println("Done after: " + ((System.currentTimeMillis() - startMs) / 1000) + " seconds.");
     }
 
 }
--- src/test/java/com/dlmol/dirmigrationutil/DirmigrationutilApplicationTests.java
+++ src/test/java/com/dlmol/dirmigrationutil/DirmigrationutilApplicationTests.java
@@ -1,16 +1,50 @@
 package com.dlmol.dirmigrationutil;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DirmigrationutilApplicationTests {
+public class DirMigrationUtilApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void readFromNas() throws IOException {
+//        File source = new File("/Volumes/SystemStuff/Windows 7/en_windows_7_professional_with_sp1_x64_dvd_u_676939.iso"); // 3GB
+        File source = new File("/Volumes/Videos/GOPR0107_1492876862683_high.MP4"); // 179 MB
+        File targetDir = new File("/Users/dmolineu/Desktop/");
+        testCopyFile(source, targetDir);
+    }
+
+    @Test
+    public void writeToNas() throws IOException {
+        File source = new File("/Users/dmolineu/Movies/Family 001.avi");
+        File targetDir = new File("/Volumes/Temp/");
+        testCopyFile(source, targetDir);
+    }
+
+    private void testCopyFile(File source, File targetDir) throws IOException {
+        assertTrue(source.getAbsolutePath() + " NOT found!", source.isFile());
+        assertTrue(targetDir.getAbsolutePath() + " is NOT a valid directory!", targetDir.isDirectory());
+        File targetFile = new File (targetDir.getAbsolutePath() + File.separator + source.getName());
+        assertFalse(targetFile.getAbsolutePath() + " already exists!", targetFile.exists());
+        System.out.println("Starting copy of " + (source.length() / 1000) + " KB file from " + source.getAbsolutePath() +
+                "  to " + targetDir.getAbsolutePath());
+        long startMs = System.currentTimeMillis();
+        FileUtils.copyToDirectory(source, targetDir);
+        final long durationMs = System.currentTimeMillis() - startMs;
+        System.out.println("Copy done in " + durationMs + " ms, " + (source.length() / durationMs) + " KB/s");
+        assertTrue(targetFile.getAbsolutePath() + " does NOT actually exist!", targetFile.exists());
+        assertEquals("source size is " + source.length() + ", but target size is " + targetFile.length(),
+                source.length(), targetFile.length());
+        assertTrue("Unable to delete: " + targetFile.getAbsolutePath(), targetFile.delete());
     }
 
 }
